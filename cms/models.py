6204060ae81407ec08a32f133b3e397fae738002
andrey-zubov <taac@mail.ru>
2020-08-16 20:31:44 +0300

    test base without dublicate

--- cms/models.py
+++ cms/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-
 from django.utils.translation import gettext_lazy as _
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.utils.safestring import mark_safe
+
+from os.path import join
 
 from feincms.module.page.models import Page
 from feincms.contents import RichTextContent
 from feincms.module.medialibrary.contents import MediaFileContent
+from feincms.module.medialibrary.fields import MediaFileForeignKey, MediaFile
+
+from mptt import register
+from mptt.models import MPTTModel, TreeForeignKey
 
 Page.register_extensions(
     'feincms.extensions.datepublisher',
@@ -13,7 +21,7 @@ Page.register_extensions(
 
 Page.register_templates({
     'title': _('Standard template'),
-    'path': 'main_article.html',
+    'path': 'base_0.html',
     'regions': (
         ('main', _('Main content area')),
         ('sidebar', _('Sidebar'), 'inherited'),
@@ -25,3 +33,40 @@ Page.create_content_type(MediaFileContent, TYPE_CHOICES=(
     ('default', _('default')),
     ('lightbox', _('lightbox')),
 ))
+
+
+class Column(models.Model):
+    title = models.CharField(
+        verbose_name='Название виджета',
+        max_length=256,
+    )
+    picture = MediaFileForeignKey(
+        MediaFile,
+        on_delete=models.SET_NULL,
+        verbose_name='Картинка',
+        null=True,
+    )
+    pic_text = models.CharField(
+        verbose_name='Подпись катринки',
+        max_length=256
+    )
+    text = models.TextField(
+        verbose_name='основной текст',
+    )
+
+    class Meta:
+        verbose_name = 'Одноколоночный'
+        abstract = True
+
+    def render(self):
+        return render_to_string(
+            'main_article.html',
+            context={
+                'widget': self,
+                'text': mark_safe(self.text)
+            })
+
+    def get_img(self):
+        return join(settings.MEDIA_URL, str(self.picture.file))
+
+Page.create_content_type(Column)
