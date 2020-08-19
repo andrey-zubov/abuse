593e66a9ef173ebaed676332427b31403fd144b6
andrey-zubov <taac@mail.ru>
2020-08-19 20:23:22 +0300

    side ancors

--- cms/views.py
+++ cms/views.py
@@ -3,12 +3,18 @@ from django.http import HttpResponse
 
 from .models import WhereFindHelp, Page, Block
 
+
 def main_page(request):
     return render(request, template_name='main_page.html')
+
 
 def find_help(request):
     articles = WhereFindHelp.objects.filter(is_active=True)
     return render(request, template_name='org.html', context={'articles':articles})
+
+
+def co_dependent(request):
+    return HttpResponse('codep')
 
 
 def wtf(request):
