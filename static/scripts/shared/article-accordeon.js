08434b97f9eeb91fd8f10b1998de6c3b16658174
Uberlegsam <konstantin.kizeew@gmail.com>
2020-08-22 16:24:01 +0300

    Add smooth accordeon

--- static/scripts/shared/article-accordeon.js
+++ static/scripts/shared/article-accordeon.js
@@ -1,48 +1,33 @@
 export default class ArticleAccordeon{
     constructor(){
-        this.accordeonItems = document.querySelectorAll('.accordeon-item__title');
-        this.accordeonItemsLinks = document.querySelectorAll('.accordeon-item__link__title');
         this.init();
     }
 
-    delActiveClass(item){
-        item.parentElement.classList.remove("accordeon-item__link--active");
-    }
-
-    addActiveClassToLink(e){
-        this.accordeonItemsLinks.forEach(item=>{
-            if(item != e.currentTarget){
-                this.delActiveClass(item)
-            }
-        });
-
-        e.currentTarget.parentElement.classList.toggle("accordeon-item__link--active")
-
-    }
-
-    addActiveClass(e){
-        this.accordeonItems.forEach(item=>{
-            if(item != e.currentTarget){
-                item.parentElement.classList.remove("accordeon-item--active")
-            }
-        });
-
-        this.accordeonItemsLinks.forEach(item=>{    
-            this.delActiveClass(item)
-        });
-
-        e.currentTarget.parentElement.classList.toggle("accordeon-item--active")
-
-    }
+  
 
     init(){
-        this.accordeonItems.forEach(item=>{
-            item.addEventListener('click', (e)=>{this.addActiveClass(e)}
-        )});
 
-        this.accordeonItemsLinks.forEach(item=>{
-            item.addEventListener('click', (e)=>{this.addActiveClassToLink(e)}
-        )});
+        $('.accordeon-item__links').hide();
+  
+        $('.accordeon-item__title').click(function(){
+          $(this).parent().toggleClass('accordeon-item--active').siblings().removeClass('accordeon-item--active');
+          $('.accordeon-item__links').slideUp();
+          $('.accordeon-item__links').children().children('.accordeon-item__link__content').slideUp();
+          
+          if(!$(this).next().is(":visible")) {
+                $(this).next().slideDown();
+              }
+        });
+
+        $('.accordeon-item__link__content').hide();
+  
+        $('.accordeon-item__link__title').click(function(){
+          $('.accordeon-item__link__content').slideUp();
+          
+          if(!$(this).next().is(":visible")) {
+                  $(this).next().slideDown();
+              }
+        });
 
     }
 }
