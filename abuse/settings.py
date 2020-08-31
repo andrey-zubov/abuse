9793a0bd16b53c2e8dae38e3ffdaee97be1dbf14
andrey-zubov <taac@mail.ru>
2020-08-31 20:23:56 +0300

    hosting settings

--- abuse/settings.py
+++ abuse/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'o%purl422v$reg*1m#sujh$)=n!f#ojw83v2uv+=9m@mn9i=za'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taac.pythonanywhere.com']
 
 
 # Application definition
