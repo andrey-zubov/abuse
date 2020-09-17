12d86943ccc35ff69364add94ecc51a2c9885632
andrey-zubov <taac@mail.ru>
2020-09-17 18:15:34 +0300

    api id

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 class OrgSerializer(serializers.Serializer):
-    title  = serializers.CharField(max_length=120)
+    id = serializers.CharField()
+    title = serializers.CharField(max_length=120)
     city = serializers.CharField(max_length=120)
     adress = serializers.CharField(max_length=120)
     lat = serializers.CharField(max_length=120)
